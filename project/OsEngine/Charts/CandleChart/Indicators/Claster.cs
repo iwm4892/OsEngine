dcb969f452f585aa56be6cbc68273fec4a5a79d6
Русин Дмитрий Александрович <ragnobot@gmail.com>
2018-05-17 14:58:21 +0300

    Подготовка к фут принт

---
+++ project/OsEngine/Charts/CandleChart/Indicators/Claster.cs
@@ -0,0 +1,360 @@
+﻿/*
+ *Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using OsEngine.Entity;
+
+namespace OsEngine.Charts.CandleChart.Indicators
+{
+    /// <summary>
+    ///  Volume. Объём свечек. Индикатор
+    /// </summary>
+    public class Claster:IIndicatorCandle
+    {
+
+        /// <summary>
+        /// конструктор с параметрами. Индикатор будет сохраняться
+        /// </summary>
+        /// <param name="uniqName">уникальное имя</param>
+        /// <param name="canDelete">можно ли пользователю удалить индикатор с графика вручную</param>
+        public Claster(string uniqName,bool canDelete)
+        {
+            Name = uniqName;
+            TypeIndicator = IndicatorOneCandleChartType.Column;
+            ColorBase = Color.DeepSkyBlue;
+            ColorUp = Color.DodgerBlue;
+            ColorDown = Color.DarkRed;
+            PaintOn = false;
+            CanDelete = canDelete;
+            Load();
+        }
+
+        /// <summary>
+        /// конструктор без параметров. Индикатор не будет сохраняться
+        /// используется ТОЛЬКО для создания составных индикаторов
+        /// не используйте его из слоя создания роботов!
+        /// </summary>
+        /// <param name="canDelete">можно ли пользователю удалить индикатор с графика вручную</param>
+        public Claster(bool canDelete)
+        {
+            Name = Guid.NewGuid().ToString();
+            TypeIndicator = IndicatorOneCandleChartType.Column;
+            ColorBase = Color.DeepSkyBlue;
+            
+            ColorUp = Color.DodgerBlue;
+            ColorDown = Color.DarkRed;
+            
+            PaintOn = false;
+            CanDelete = canDelete;
+        }
+        
+        /// <summary>
+        /// Данные кластеров
+        /// </summary>
+        public List<ClasterData> data;
+        /// <summary>
+        /// все значения индикатора
+        /// </summary>
+        List<List<decimal>> IIndicatorCandle.ValuesToChart
+        {
+            get
+            {
+                List<List<decimal>> list = new List<List<decimal>>();
+                list.Add(Values);
+                return list;
+            }
+        }
+
+        /// <summary>
+        /// цвета для индикатора
+        /// </summary>
+        List<Color> IIndicatorCandle.Colors
+        {
+            get
+            {
+                List<Color> colors = new List<Color>();
+                //colors.Add(ColorBase);
+                
+                colors.Add(ColorUp);
+                colors.Add(ColorDown);
+                
+                return colors;
+            }
+
+        }
+
+        /// <summary>
+        /// можно ли удалить индикатор с графика. Это нужно для того чтобы у роботов нельзя было удалить 
+        /// индикаторы которые ему нужны в торговле
+        /// </summary>
+        public bool CanDelete { get; set; }
+
+        /// <summary>
+        /// тип индикатора
+        /// </summary>
+        public IndicatorOneCandleChartType TypeIndicator
+        { get; set; }
+
+        /// <summary>
+        /// имя серии данных на которой будет прорисовываться индикатор
+        /// </summary>
+        public string NameSeries
+        { get; set; }
+
+        /// <summary>
+        /// имя области данных на которой будет прорисовываться индикатор
+        /// </summary>
+        public string NameArea
+        { get; set; }
+
+        /// <summary>
+        /// объём
+        /// </summary>
+        public List<decimal> Values
+        { get; set; }
+
+        /// <summary>
+        /// уникальное имя
+        /// </summary>
+        public string Name
+        { get; set; }
+
+        
+        /// <summary>
+        /// цвет растущего объёма
+        /// </summary>
+        public Color ColorUp
+        { get; set; }
+
+        /// <summary>
+        /// цвет падающего объёма
+        /// </summary>
+        public Color ColorDown
+        { get; set; }
+        
+
+        /// <summary>
+        /// цвет линии индикатора
+        /// </summary>
+        public Color ColorBase { get; set; }
+
+        /// <summary>
+        /// включена ли прорисовка индикатора на чарте
+        /// </summary>
+        public bool PaintOn
+        { get; set; }
+        /// <summary>
+        /// Цвета значений
+        /// </summary>
+        public List<Color> ColorSeries { get; set; }
+
+        /// <summary>
+        /// сохранить настройки в файл
+        /// </summary>
+        public void Save()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(Name))
+                {
+                    return;
+                }
+                using (StreamWriter writer = new StreamWriter(@"Engine\" + Name + @".txt", false))
+                {
+                    writer.WriteLine(ColorBase.ToArgb());
+                    
+                    writer.WriteLine(ColorUp.ToArgb());
+                    writer.WriteLine(ColorDown.ToArgb());
+                    
+                    writer.WriteLine(PaintOn);
+                    writer.Close();
+                }
+            }
+            catch (Exception)
+            {
+                // отправить в лог
+            }
+        }
+
+        /// <summary>
+        /// загрузить настройки из файла
+        /// </summary>
+        public void Load()
+        {
+            if (!File.Exists(@"Engine\" + Name + @".txt"))
+            {
+                return;
+            }
+            try
+            {
+                using (StreamReader reader = new StreamReader(@"Engine\" + Name + @".txt"))
+                {
+                    ColorBase = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
+                    
+                    ColorUp = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
+                    ColorDown = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
+                    
+                    PaintOn = Convert.ToBoolean(reader.ReadLine());
+                    reader.Close();
+                }
+            }
+            catch (Exception)
+            {
+                // отправить в лог
+            }
+        }
+
+        /// <summary>
+        /// удалить файл с настройками
+        /// </summary>
+        public void Delete()
+        {
+            if (File.Exists(@"Engine\" + Name + @".txt"))
+            {
+                File.Delete(@"Engine\" + Name + @".txt");
+            }
+        }
+
+        /// <summary>
+        /// удалить данные
+        /// </summary>
+        public void Clear()
+        {
+            if (Values != null)
+            {
+                Values.Clear();
+            }
+        }
+
+        /// <summary>
+        /// показать окно настроек
+        /// </summary>
+        public void ShowDialog()
+        {
+        /*    VolumeUi ui = new VolumeUi(this);
+            ui.ShowDialog();
+
+            if (ui.IsChange)
+            {
+                if (NeadToReloadEvent != null)
+                {
+                    NeadToReloadEvent(this);
+                }
+            }
+            */
+        }
+
+        /// <summary>
+        /// нужно перерисовать индикатор
+        /// </summary>
+        public event Action<IIndicatorCandle> NeadToReloadEvent;
+
+// вычисления
+
+        /// <summary>
+        /// прогрузить индикатор свечками
+        /// </summary>
+        public void Process(List<Candle> candles)
+        {
+            if (Values != null &&
+                           Values.Count + 1 == candles.Count)
+            {
+                ProcessOneCandle(candles);
+            }
+            else if (Values != null &&
+                Values.Count == candles.Count)
+            {
+                ProcessLastCanlde(candles);
+            }
+            else
+            {
+                ProcessAllCandle(candles);
+            }
+        }
+
+        /// <summary>
+        /// прогрузить только последнюю свечку
+        /// </summary>
+        private void ProcessOneCandle(List<Candle> candles)
+        {
+            if (Values == null)
+            {
+                Values = new List<decimal>();
+                ColorSeries = new List<Color>();
+            }
+            ClasterData clasterData = GetValue(candles, candles.Count - 1);
+            data.Add(clasterData);
+            Values.Add(clasterData.MaxData.prise);
+            ColorSeries.Add(GetColor(clasterData));
+        }
+
+        /// <summary>
+        /// прогрузить все свечи
+        /// </summary>
+        private void ProcessAllCandle(List<Candle> candles)
+        {
+            Values = new List<decimal>();
+            ColorSeries = new List<Color>();
+
+            for (int i = 0; i < candles.Count; i++)
+            {
+                ClasterData clasterData = GetValue(candles, i);
+                data.Add(clasterData);
+
+                Values.Add(clasterData.MaxData.prise);
+                ColorSeries.Add(GetColor(clasterData));
+            }
+        }
+
+        /// <summary>
+        /// перегрузить последнюю свечу
+        /// </summary>
+        private void ProcessLastCanlde(List<Candle> candles)
+        {
+            ClasterData clasterData = GetValue(candles, candles.Count - 1);
+            data[data.Count - 1] = clasterData;
+
+            Values[Values.Count-1]  = clasterData.MaxData.prise;
+            ColorSeries[ColorSeries.Count - 1] = GetColor(clasterData);
+        }
+        /// <summary>
+        /// Получить цвет по значению
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        private Color GetColor(ClasterData val)
+        {
+            if (val.MaxData.side == Side.Buy)
+            {
+                return ColorUp;
+            }
+            return ColorDown;
+        }
+        /// <summary>
+        /// взять значение индикаторм по индексу
+        /// </summary>
+        private ClasterData GetValue(List<Candle> candles, int index)
+        {
+            if (index > candles.Count-1)
+            {
+                return new ClasterData();
+            }
+
+            List<Trade> trades = candles[index].Trades;
+
+            if (trades == null ||
+                trades.Count == 0)
+            {
+                return new ClasterData();
+            }
+
+            ClasterData data = new ClasterData(trades);
+            return data;        
+        }
+
+    }
+}
---
+++ project/OsEngine/Entity/ClasterData.cs
@@ -0,0 +1,119 @@
+﻿/*
+ *Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using System;
+using OsEngine.Market.Servers.Entity;
+using System.Collections.Generic;
+
+namespace OsEngine.Entity
+{
+    /// <summary>
+    /// Данные одной свечи
+    /// </summary>
+    public class ClasterData
+    {
+        public ClasterData(List<Trade> trades)
+        {
+            update(trades);
+        }
+        public ClasterData()
+        {
+        }
+        public void update(List<Trade> trades)
+        {
+            data.Clear();
+            for (int i = 0; i < trades.Count; i++)
+            {
+                add(trades[i]);
+            }
+            decimal max=0;
+
+            for(int i = 0; i < data.Count; i++)
+            {
+                if (data[i].volume > max)
+                {
+                    MaxData = data[i];
+                }
+            }
+        }
+        /// <summary>
+        /// данные объемов по ценам свечи
+        /// </summary>
+        List<PriseData> data;
+        /// <summary>
+        /// Добавление данных в колекцию
+        /// </summary>
+        public void add(Trade trade)
+        {
+            //дозаполняем накопленные цены
+            for (int i = 0;i<data.Count;i++)
+            {
+                if (data[i].prise == trade.Price)
+                {
+                    data[i].Add(trade);
+                    return;
+                }
+            }
+            // добавляем новые цены
+            PriseData _dt = new PriseData();
+            _dt.prise = trade.Price;
+            _dt.Add(trade);
+            data.Add(_dt);
+        }
+        /// <summary>
+        /// Направление максимального объема Buy/Sell
+        /// </summary>
+        public PriseData MaxData;
+
+        /// <summary>
+        /// данные конкретной цены
+        /// </summary>
+        public struct PriseData
+        {
+            /// <summary>
+            /// цена
+            /// </summary>
+            public decimal prise;
+            /// <summary>
+            /// объем
+            /// </summary>
+            public decimal volume;
+            /// <summary>
+            /// направление
+            /// </summary>
+            public Side side;
+            /// <summary>
+            /// объем на покупку
+            /// </summary>
+            private decimal volumeBuy;
+            /// <summary>
+            /// объем на продажу
+            /// </summary>
+            private decimal volumeSell;
+
+
+            public void Add(Trade trade)
+            {
+                if (trade.Side == Side.Buy)
+                {
+                    volumeBuy += trade.Volume;
+                }
+                else
+                {
+                    volumeSell += trade.Volume;
+                }
+                volume = Math.Max(volumeBuy, volumeSell);
+                if(volume== volumeBuy) {
+                    side = Side.Buy;
+                }
+                else
+                {
+                    side = Side.Sell;
+                }
+
+            }
+
+        }
+    }
+}
